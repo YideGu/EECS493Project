4ff4549699d8bdc0ff989a72ca46db1a9b67f93a
Yide Gu <guyide@umich.edu>
2020-11-18 23:10:45 -0500

    Adjust css for search feature.

--- scripts/page.js
+++ scripts/page.js
@@ -24,7 +24,7 @@ function myFunction() {
   
     for (i = 0; i < nodes.length; i++) {
       if (nodes[i].innerText.toLowerCase().includes(filter)) {
-        nodes[i].style.display = "block";
+        nodes[i].style.display = "inline-block";
       } else {
         nodes[i].style.display = "none";
       }
