c1d939621612950ef6f48d05714b0f2e59ca7a97
Yide Gu <guyide@umich.edu>
2020-12-03 15:32:38 -0500

    Start using tamplate and moved some responsive function from mainPage.js to page.js

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,30 +1,4 @@
 
-$('.imgContainer').click(function() {
-	// redirect to the buyer page
-	window.location.href='buyerPage.html';
-
-	// store the item info in local storage
-	var img_src = $(this).find('img').attr('src');
-	localStorage.setItem('img_src', img_src);
-
-	var item_name = $(this).find('h1').text();
-	localStorage.setItem('item_name', item_name);
-
-	var item_price = $(this).find('p').eq(0).text();
-	localStorage.setItem('item_price', item_price);
-	console.log(item_price);
-
-	var item_description = $(this).find('p').eq(1).text();
-	localStorage.setItem('item_description', item_description);
-	console.log(item_description);
-
-	var delivery_option = $(this).find('p').eq(2).text();
-	localStorage.setItem('delivery_option', delivery_option);
-	console.log(delivery_option);
-})
-
-
-
 $('.close').click(function(){
     $(this).parent().remove();
 })
