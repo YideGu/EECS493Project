271f7f9e4fe56cd332e921a647844d00f660ddb1
Yide Gu <guyide@umich.edu>
2020-12-03 20:02:20 -0500

    Modified sell item description

--- scripts/data.js
+++ scripts/data.js
@@ -3,43 +3,50 @@ var myData = [
         Thumbnail: "images\\00Z0Z_8hggfGA06si_0CI0t2_600x450.jpg", 
         itemTitle: "Wooden table with four chairs", price: "150", 
         description: "Brand new round wooden table with corresponding chair.",
-        delivery: "pick up only."
+        delivery: "pick up only.",
+        seller: "bot001@umich.edu"
     },
     {
         Thumbnail: "images\\00B0B_9fVMEesNeVP_600x450.jpg",  
         itemTitle: " 7 NEW -GE Biax T/E ECO 26 W 4 PIN CFL Bulbs", price: "25", 
         description: "New, 7 NEW -GE Biax T/E ECO 26 W 4 PIN CFL Bulbs.",
-        delivery: "pick up only."
+        delivery: "Ann Arbor.",
+        seller: "bot001@umich.edu"
     },
     {
         Thumbnail: "images\\00i0i_hKD0SWvt1zv_0hq0oT_600x450.jpg",  
         itemTitle: "Vanity", price: "150", 
         description: "Brand new round wooden table with corresponding chair.",
-        delivery: "pick up only."
+        delivery: "North Campus.",
+        seller: "bot001@umich.edu"
     },
     {
         Thumbnail: "images\\00x0x_ca27E3fqEp4_09G06s_600x450.jpg",  
         itemTitle: "Wooden table with four chairs", price: "150", 
         description: "Brand new round wooden table with corresponding chair.",
-        delivery: "pick up only."
+        delivery: "Central Campus.",
+        seller: "bot001@umich.edu"
     },
     {
         Thumbnail: "images\\01515_6yyMIr4hDVG_600x450.jpg",  
         itemTitle: "HP 64 Ink Cartridge 2 Pack Brand New!", price: "19", 
         description: "Brand New in Box. Never open or used. Paid $40+. Bought new printer.",
-        delivery: "pick up only."
+        delivery: "Within 10 miles of North Campus",
+        seller: "bot001@umich.edu"
     },
     {
         Thumbnail: "images\\00o0o_cFieU7ZLchn_0BC0t2_600x450.jpg",  
         itemTitle: "Wooden table with four chairs", price: "150", 
         description: "Brand new round wooden table with corresponding chair.",
-        delivery: "pick up only."
+        delivery: "pick up only.",
+        seller: "bot002@umich.edu"
     },
     {
         Thumbnail: "images\\00A0A_ac81hm0nSDP_0pO0jm_600x450.jpg",  
         itemTitle: "Wooden table with four chairs", price: "150", 
         description: "Brand new round wooden table with corresponding chair.",
-        delivery: "pick up only."
+        delivery: "pick up only.",
+        seller: "bot003@umich.edu"
     },
     {
         Thumbnail: "images\\00d0d_bjey2VzPXAk_063084_600x450.jpg",  
@@ -53,7 +60,8 @@ var myData = [
         "Local pickup near Ann Arbor, Michigan, at the McDonald's at 373 N Zeeb Road at I-94, exit 169. Shipping is also available for the added cost of shipping, insurance, and PayPal fees." +
 
         "Thanks for looking!",
-        delivery: "pick up only."
+        delivery: "pick up only.",
+        seller: "bot003@umich.edu"
     },
     {
         Thumbnail: "images\\00808_9EPe2rVmbpX_09e09q_600x450.jpg",  
@@ -64,7 +72,8 @@ var myData = [
         "Raised lips around camera and screen to stay untouched"+
         "Our case is designed to be compatible with a Spigen brand screen protector"+
         "iPad Pro 12.9 2018 Case only Compatible with iPad Pro 12.9 inch Case (2018). Not Compatible with iPad Pro 12.9 2020/2017/2015 and other iPad models",
-        delivery: "pick up only."
+        delivery: "pick up only.",
+        seller: "bot004@umich.edu"
     }
 ];
 
