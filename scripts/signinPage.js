6ad17a1f0b736a5758f71c83db24eaef45ef05d8
yichansu <yichansu@umich.edu>
2020-12-04 15:05:41 -0500

    updated signin page style

--- scripts/signinPage.js
+++ scripts/signinPage.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
 
     window.location.href='index.html';
   }
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+    });
+  }
+function onFailure(error) {
+    alert("Please sign in with your UM account")
+    }
+
 })
